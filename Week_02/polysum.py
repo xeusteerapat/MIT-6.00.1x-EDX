59a15d5177d89a5a0ac399fcfe852788f6806d2e
xeusteerapat <xeus085@gmail.com>
2019-09-09 22:16:46 +0700

    added week 02 problems

--- Week_02/polysum.py
+++ Week_02/polysum.py
@@ -3,8 +3,7 @@ from math import tan, pi
 
 def polysum(n, s):
     radians = pi / n
-    tan_term = tan(radians)
-    regular_area = (0.25 * n * (s**2)) / tan_term
+    regular_area = (0.25 * n * (s**2)) / tan(radians)
     perimeter = s * n
     answer = regular_area + perimeter ** 2
     answer = round(answer, 4)
