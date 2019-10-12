8cff5de2e6a22eb87f30e2d51a56ea58354e41cf
xeusteerapat <xeus085@gmail.com>
2019-10-12 16:47:21 +0700

    deleted unnecessary file

--- Week_05/OOP_Part_02/class_birthday.py
+++ Week_05/OOP_Part_02/class_birthday.py
@@ -50,3 +50,71 @@ print('-------------')
 person_list.sort()
 for ele in person_list:
     print(ele)
+
+
+class MITPerson(Person):
+    # class variable
+    next_id_num = 0
+
+    def __init__(self, name):
+        # using init method from parent class
+        Person.__init__(self, name)
+        self.id_num = MITPerson.next_id_num
+        MITPerson.next_id_num += 1
+
+    def get_id_num(self):
+        return self.id_num
+
+    def __lt__(self, other):
+        return self.id_num < other.id_num
+
+    def speak(self, utterance):
+        return (self.get_lastname() + ' says: ' + utterance)
+
+
+m3 = MITPerson("Mark Zuckerburg")
+Person.set_birthday(m3, 5, 14, 84)
+
+m2 = MITPerson("Drew Houston")
+Person.set_birthday(m2, 3, 14, 83)
+
+m1 = MITPerson("Bill Gates")
+Person.set_birthday(m1, 10, 28, 55)
+
+MITPerson_list = [m1, m2, m3]
+
+print(m3)
+print(m3.speak("I'm invester"))
+
+# Create Undergraduate MIT class which is inherit from MITPerson
+
+
+class UG(MITPerson):
+    def __init__(self, name, classYear):
+        # using MITPerson init method
+        MITPerson.__init__(self, name)
+        self.year = classYear
+
+    def get_class(self):
+        return self.year
+
+    def speak(self, utterance):
+        return MITPerson.speak(self, " Dude, " + utterance)
+
+# Create class Grad which is inherit from MITPerson
+
+
+class Grad(MITPerson):
+    pass
+
+
+def isStudent(obj):
+    return isinstance(obj, UG) or isinstance(obj, Grad)
+
+
+s1 = UG("Matt Damon", 2017)
+s2 = UG("Ben Affleck", 2017)
+s3 = UG("Lin Miranda", 2018)
+s4 = Grad("Leonardo Di Carpio")
+
+student_list = [s1, s2, s3, s4]
