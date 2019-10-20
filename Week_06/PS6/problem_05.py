9461e615069b9157e9aca0e9a08497577f53c41c
xeusteerapat <xeus085@gmail.com>
2019-10-20 16:00:48 +0700

    added pset 6

---
+++ Week_06/PS6/problem_05.py
@@ -0,0 +1,21 @@
+def search(L, e):
+    for i in range(len(L)):
+        if L[i] == e:
+            return True
+        if L[i] > e:
+            return False
+    return False
+
+
+def newsearch(L, e):
+    size = len(L)
+    for i in range(size):
+        if L[size-i-1] == e:
+            return True
+        if L[i] < e:
+            return False
+    return False
+
+
+print(search([1, 2], 2))
+print(newsearch([1, 2], 2))
